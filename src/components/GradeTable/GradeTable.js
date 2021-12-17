70ce713ad13b2b3dbb8b33d199f43ab5dc806848
hieutruong455 <55783505+hieutruong455@users.noreply.github.com>
2021-12-17 11:06:15 +0700

    gradeboard_v3

--- src/components/GradeTable/GradeTable.js
+++ src/components/GradeTable/GradeTable.js
@@ -1,63 +1,101 @@
-import React from 'react'
-import { forwardRef } from 'react';
-import MaterialTable from 'material-table'
-import { Search,AddBox,ArrowDownward,Check,ChevronLeft,ChevronRight,Clear,DeleteOutline,Edit,FilterList,FirstPage,LastPage,Remove,SaveAlt,ViewColumn } from '@mui/icons-material'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { 
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+  gridClasses, 
+} from '@mui/x-data-grid';
 
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
 
-export default function CellEditable() {
-  const { useState } = React;
-
-  const [columns, setColumns] = useState([
-    { title: 'Name', field: 'name', cellStyle: {minWidth: 150} },
-    { title: 'Grade', field: 'grade', cellStyle: {minWidth: 150}, initialEditValue: 'initial edit value' },
-    { title: 'Grade1', field: 'grade1', cellStyle: {minWidth: 150}, initialEditValue: 'initial edit value' },
-    { title: 'Grade2', field: 'grade2', cellStyle: {minWidth: 150}, initialEditValue: 'initial edit value' },
-    { title: 'Grade3', field: 'grade3', cellStyle: {minWidth: 150}, initialEditValue: 'initial edit value' },
-    { title: 'Grade4', field: 'grade4', cellStyle: {minWidth: 150}, initialEditValue: 'initial edit value' },
-
-  ]);
-
-  const [data, setData] = useState([
-    { name: 'Hieu', grade: '10', grade1: '9', grade2: '9', grade3: '9', grade4: '9' },
-    { name: 'Danh', grade: '10', grade1: '9', grade2: '9', grade3: '9', grade4: '9' },
-    { name: 'Duc', grade: '10', grade1: '9', grade2: '9', grade3: '9', grade4: '9' },
-  ]);
-
+export default function GradeTable() {
+  const columns = [
+    { field: 'name', headerName: 'Name', width: 180, editable: true },
+    { field: 'grade', headerName: 'Grade', type: 'number', editable: true },
+    { field: 'grade1', headerName: 'Grade1', type: 'number', editable: true },
+    { field: 'grade2', headerName: 'Grade2', type: 'number', editable: true },
+    { field: 'grade3', headerName: 'Grade3', type: 'number', editable: true },
+    { field: 'grade4', headerName: 'Grade', type: 'number', editable: true },
+  ];
+  
+  
+  const rows = [
+    {
+      id: 1,
+      name: "Hieu",
+      grade: 10,
+      grade1: 5,
+      grade2: 8,
+      grade3: 9,
+      grade4: 7,
+    },
+    {
+      id: 2,
+      name: "Duc",
+      grade: 9,
+      grade1: 8,
+      grade2: 7,
+      grade3: 9,
+      grade4: 10,
+    },
+    {
+      id: 3,
+      name: "Danh",
+      grade: 6,
+      grade1: 10,
+      grade2: 8,
+      grade3: 9,
+      grade4: 9,
+    },
+    {
+      id: 4,
+      name: "Dung",
+      grade: 8,
+      grade1: 9,
+      grade2: 8,
+      grade3: 9,
+      grade4: 9,
+    },
+    {
+      id: 5,
+      name: "Bao",
+      grade: 8,
+      grade1: 8,
+      grade2: 8,
+      grade3: 6,
+      grade4: 7,
+    },
+  ];
+  
   return (
-    <MaterialTable
-      title="Cell Editable Preview"
-      columns={columns}
-      data={data}
-      options={{
-        exportButton:true,
+    <Box
+      sx={{
+        height: 2000,
+        width: 1,
+        '& .cold': {
+          backgroundColor: '#b9d5ff91',
+          color: '#1a3e72',
+        },
+        '& .hot': {
+          backgroundColor: '#ff943975',
+          color: '#1a3e72',
+        },
       }}
-      cellEditable={{
-        onCellEditApproved: (newValue, oldValue, rowData, columnDef) => {
-          return new Promise((resolve, reject) => {
-            console.log('newValue: ' + newValue);
-            setTimeout(resolve, 1000);
-          });
-        }
-      }}
-    />
-  )
+    >
+      <div style={{ height: 2000, width: '100%' }}>
+        <DataGrid 
+   
+        rows={rows} 
+        columns={columns} 
+        getCellClassName={(params) => {
+          if (params.field === 'name') {
+            return '';
+          }
+          return params.value >= 7 ? 'hot' : 'cold';
+        }}
+        />
+      </div>
+    </Box>
+  );
 }
+
